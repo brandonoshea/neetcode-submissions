0082d71493cd418cdacd90ec19188ecb8ff5cbb0
brandonoshea <bfoshea@clemson.edu>
2026-09-21 18:57:21 -0400

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            mergedList = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                if (i + 1) < len(lists):
+                    l2 = lists[i + 1]
+                else:
+                    l2 = None
+                mergedList.append(self.mergeList(l1, l2))
+
+            lists = mergedList
+
+        return lists[0]
+
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+        # attach whichever list has leftover nodes, once, after the loop
+        tail.next = l1 if l1 else l2
+
+        return dummy.next
